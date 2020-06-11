3f13359e4adfed456eaa559347e87c3b6b7a7f7f
aledeltoro <alejandroencarnacion1@gmail.com>
2020-06-11 14:36:05 -0400

    ref: Rewrite tests based on TDD

--- numberline/range.go
+++ numberline/range.go
@@ -1,9 +1,9 @@
 package numberline
 
 import (
+	"strconv"
 	"errors"
 	"log"
-	"strconv"
 )
 
 // Range data type
@@ -12,28 +12,29 @@ type Range struct {
 }
 
 var (
-	errInvalidRange error = errors.New("Error: Invalid range limits")
-	errInvalidValue error = errors.New("Error: Invalid value")
+	errInvalidRange error = errors.New("error: invalid range limits")
+	errInvalidValue error = errors.New("error: invalid value")
 )
+
 
 // NewRange creates a new instance of type Range
 func (r Range) NewRange(expression string) (Range, error) {
-	var numRange Range
-
+	
 	validLowerRange := [2]byte{40, 91}
 	validUpperRange := [2]byte{41, 93}
 
-	var isLowerRange bool = contains(validLowerRange, expression[0])
-	var isUpperRange bool = contains(validUpperRange, expression[len(expression)-1])
+	isLowerRange := contains(expression[0], validLowerRange)
+	isUpperRange := contains(expression[len(expression) - 1], validUpperRange)
 
 	if !(isLowerRange && isUpperRange) {
-		return numRange, errInvalidRange
+		return Range{}, errInvalidRange
 	}
 
-	var lowerBound, upperBound string
 	var isLowerBound bool = true
 
-	for i := 1; i < len(expression)-1; i++ {
+	var lowerBound, upperBound string
+
+	for i := 1; i < len(expression) - 1; i++ {
 		if expression[i] == 44 {
 			isLowerBound = false
 		} else if isLowerBound {
@@ -45,30 +46,30 @@ func (r Range) NewRange(expression string) (Range, error) {
 
 	lowerLimit, err := strconv.Atoi(lowerBound)
 	if err != nil {
-		return numRange, errInvalidValue
+		return Range{}, errInvalidValue
 	}
 
 	upperLimit, err := strconv.Atoi(upperBound)
 	if err != nil {
-		return numRange, errInvalidValue
+		return Range{}, errInvalidValue
 	}
 
-	if expression[0] == validLowerRange[0] {
+	if expression[0] == 40 {
 		lowerLimit++
 	}
 
-	if expression[len(expression)-1] == validUpperRange[0] {
+	if expression[len(expression) - 1] == 41 {
 		upperLimit--
 	}
 
-	numRange = Range{lowerLimit, upperLimit}
+	return Range{lowerLimit, upperLimit}, nil
 
-	return numRange, nil
+
 }
 
 // Contains checks if Range r contains all of the specified numbers
 func (r Range) Contains(numbers ...int) bool {
-
+	
 	for _, number := range numbers {
 		if !(r.LowerBound <= number && r.UpperBound >= number) {
 			return false
@@ -76,11 +77,12 @@ func (r Range) Contains(numbers ...int) bool {
 	}
 
 	return true
+
 }
 
 // DoesNotContain checks if Range r doesn't contain all of the specified numbers
 func (r Range) DoesNotContain(numbers ...int) bool {
-
+	
 	if r.Contains(numbers...) {
 		return false
 	}
@@ -89,7 +91,7 @@ func (r Range) DoesNotContain(numbers ...int) bool {
 
 }
 
-// GetAllPoints returns a slice with all the numbers inside Range r
+// GetAllPoints returns all the value inside Range r
 func (r Range) GetAllPoints() []int {
 	var points []int
 
@@ -101,7 +103,7 @@ func (r Range) GetAllPoints() []int {
 
 }
 
-// ContainsRange checks if Range r contains the specified expression
+// ContainsRange checks if Range r contains the range in the specified expression 
 func (r Range) ContainsRange(expression string) bool {
 	var comparingRange Range
 	var err error
@@ -111,9 +113,9 @@ func (r Range) ContainsRange(expression string) bool {
 		log.Fatalln(err)
 	}
 
-	var isInsideRange bool = r.LowerBound <= comparingRange.LowerBound && r.UpperBound >= comparingRange.UpperBound
+	var insideRange bool = r.LowerBound <= comparingRange.LowerBound && r.UpperBound >= comparingRange.UpperBound
 
-	if isInsideRange {
+	if insideRange {
 		return true
 	}
 
@@ -121,9 +123,9 @@ func (r Range) ContainsRange(expression string) bool {
 
 }
 
-// DoesNotContainRange checks if Range r doesn’t contain the specified expression
+// DoesNotContainRange checks if Range r doesn't contain the range in the specified expression 
 func (r Range) DoesNotContainRange(expression string) bool {
-
+	
 	if r.ContainsRange(expression) {
 		return false
 	}
@@ -136,7 +138,7 @@ func (r Range) DoesNotContainRange(expression string) bool {
 func (r Range) GetEndPoints() (lower, upper int) {
 	lower = r.LowerBound
 	upper = r.UpperBound
-	return
+	return 
 }
 
 // OverlapsRange checks if the lower and upper bound of the specified expression overlaps with Range r
@@ -160,6 +162,7 @@ func (r Range) OverlapsRange(expression string) bool {
 
 }
 
+
 // Equals checks if Range r is equal to the specified expression
 func (r Range) Equals(expression string) bool {
 	var comparingRange Range
@@ -175,22 +178,24 @@ func (r Range) Equals(expression string) bool {
 	}
 
 	return false
-
+	
 }
 
-// NotEquals checks if Range r is not equal to the specified expression
+// NotEquals checks if Range r is equal to the specified expression
 func (r Range) NotEquals(expression string) bool {
-
+	
 	if r.Equals(expression) {
 		return false
 	}
 
 	return true
+
 }
 
-func contains(byteArr [2]byte, byteChar byte) bool {
-	for _, char := range byteArr {
-		if byteChar == char {
+
+func contains(char byte, byteArr [2]byte) bool {
+	for i := range byteArr {
+		if char == byteArr[i] {
 			return true
 		}
 	}
